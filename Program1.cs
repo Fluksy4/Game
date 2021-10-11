f56765cb75ae050f017410ad6514923b98f6aba3
Dimiter Vakovsky <vakovsky@gmail.com>
2021-10-11 10:57:14 +0300

    Update Program1.cs

--- Program1.cs
+++ Program1.cs
@@ -3,11 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 namespace ConsoleApplication1
 {
     class View
